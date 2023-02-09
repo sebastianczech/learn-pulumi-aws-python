e65e75a6d0ed171b529fd6d7bb2c42e0cbffc097
Sebastian Czech <sebaczech@gmail.com>
2023-02-09 21:06:54 +0100

    Add tests to checks tags for SNS and DynamoDB

--- iac/infra.py
+++ iac/infra.py
@@ -5,10 +5,15 @@ pulumi_sqs_serverless_rest_api = sqs.Queue("pulumi_sqs_serverless_rest_api",
                                            fifo_queue=False,
                                            tags={
                                                "Environment": "development",
+                                               "Name": "pulumi_sqs_serverless_rest_api",
                                            })
 
 # Create SNS topic: https://www.pulumi.com/registry/packages/aws/api-docs/sns/topic/
-pulumi_sns_serverless_rest_api = sns.Topic("pulumi_sns_serverless_rest_api")
+pulumi_sns_serverless_rest_api = sns.Topic("pulumi_sns_serverless_rest_api",
+                                           tags={
+                                               "Environment": "development",
+                                               "Name": "pulumi_sns_serverless_rest_api",
+                                           })
 
 # Create SNS topic subscription: https://www.pulumi.com/registry/packages/aws/api-docs/sns/topicsubscription/
 pulumi_sns_topic_email_subscription = sns.TopicSubscription("pulumi_sns_topic_email_subscription",
--- iac/test_pytest_infra.py
+++ iac/test_pytest_infra.py
@@ -18,19 +18,30 @@ pulumi.runtime.set_mocks(
 import infra
 
 
+def check_tags(args):
+    urn, tags = args
+    assert tags is not None
+    assert "Environment" in tags
+    assert "Name" in tags
+
+
 @pulumi.runtime.test
 def test_queue_has_tag_name():
-    def check_tags(args):
-        urn, tags = args
-        assert tags is not None
-        assert "Environment" in tags
+    return pulumi.Output.all(infra.pulumi_sqs_serverless_rest_api.urn,
+                             infra.pulumi_sqs_serverless_rest_api.tags).apply(check_tags)
 
-    return pulumi.Output.all(infra.pulumi_sqs_serverless_rest_api.urn, infra.pulumi_sqs_serverless_rest_api.tags).apply(
-        check_tags)
 
-# TODO: tests SNS
+@pulumi.runtime.test
+def test_topic_has_tag_name():
+    return pulumi.Output.all(infra.pulumi_sns_serverless_rest_api.urn,
+                             infra.pulumi_sns_serverless_rest_api.tags).apply(check_tags)
 
-# TODO: tests DynamoDB
+
+@pulumi.runtime.test
+def test_dynamodb_has_tag_name():
+    return pulumi.Output.all(infra.pulumi_dynamodb_serverless_rest_api.urn,
+                             infra.pulumi_dynamodb_serverless_rest_api.tags).apply(check_tags)
+
 
 # TODO: Lambda - producer
 
