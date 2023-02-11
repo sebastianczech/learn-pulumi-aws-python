734058a3c57f151cf3f6962b9902795449e0ee34
Sebastian Czech <sebaczech@gmail.com>
2023-02-11 20:25:03 +0100

    Create Lambda with IAM role

--- iac/infra.py
+++ iac/infra.py
@@ -1,4 +1,5 @@
-from pulumi_aws import sqs, sns, dynamodb
+import pulumi as pulumi
+from pulumi_aws import sqs, sns, dynamodb, iam, lambda_
 
 # Create SQS queue: https://www.pulumi.com/registry/packages/aws/api-docs/sqs/queue/
 pulumi_sqs_serverless_rest_api = sqs.Queue("pulumi_sqs_serverless_rest_api",
@@ -39,4 +40,56 @@ pulumi_dynamodb_serverless_rest_api = dynamodb.Table("pulumi_dynamodb_serverless
 
 # TODO: provision Lambda - producer
 
+pulumi_iam_for_lambda_producer = iam.Role("pulumi_lambda_producer_role", assume_role_policy="""{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+""")
+
+pulumi_lambda_producer = lambda_.Function("pulumi_lambda_producer",
+                                          code=pulumi.FileArchive("files/producer.zip"),
+                                          role=pulumi_iam_for_lambda_producer.arn,
+                                          handler="producer.lambda_handler",
+                                          runtime="python3.9",
+                                          environment=lambda_.FunctionEnvironmentArgs(
+                                              variables={
+                                                  "foo": "bar",
+                                              },
+                                          ))
+
 # TODO: provision Lambda - consumer
+
+pulumi_iam_for_lambda_consumer = iam.Role("pulumi_lambda_consumer_role", assume_role_policy="""{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+""")
+
+pulumi_lambda_consumer = lambda_.Function("pulumi_lambda_consumer",
+                                          code=pulumi.FileArchive("files/consumer.zip"),
+                                          role=pulumi_iam_for_lambda_producer.arn,
+                                          handler="consumer.lambda_handler",
+                                          runtime="python3.9",
+                                          environment=lambda_.FunctionEnvironmentArgs(
+                                              variables={
+                                                  "foo": "bar",
+                                              },
+                                          ))
